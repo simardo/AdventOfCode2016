fbdaf763c6dea68e73313906ca18f11d9c52afc2
simardo <simardo@gmail.com>
2023-04-27 22:48:03 -0400

    Day 03

---
+++ Day03/Part1.cs
@@ -0,0 +1,16 @@
+﻿namespace Day03 {
+    internal class Part1 {
+
+        public Part1() {
+        }
+
+        public int exec(string input) {
+            List<string> triangles = input.Split('\n').ToList();
+
+            return triangles.Count(t => {
+                List<int> sides = t.Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+                return sides[0] < sides[1] + sides[2] && sides[1] < sides[0] + sides[2] && sides[2] < sides[0] + sides[1];
+            });
+        }
+    }
+}
---
+++ Day03/Part2.cs
@@ -0,0 +1,35 @@
+﻿namespace Day03 {
+    internal class Part2 {
+
+        public Part2() {
+        }
+
+        public int exec(string input) {
+            List<string> rows = input.Split('\n').ToList();
+
+            List<int>[] triangles = reset();
+
+            int result = 0;
+            for (int i = 0; i < rows.Count; i++) {
+                List<int> cols = rows[i].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+                for (int c = 0; c < cols.Count; c++) {
+                    triangles[c].Add(cols[c]);
+                }
+                if (triangles[0].Count % 3 == 0) {
+                    result += triangles.Count(sides => sides[0] < sides[1] + sides[2] && sides[1] < sides[0] + sides[2] && sides[2] < sides[0] + sides[1]);
+                    triangles = reset();
+                }
+            }
+
+            return result;
+        }
+
+        private List<int>[] reset() {
+            return new List<int>[] {
+                new List<int>(),
+                new List<int>(),
+                new List<int>()
+            };
+        }
+    }
+}
