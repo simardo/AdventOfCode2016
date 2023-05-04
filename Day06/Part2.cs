4a97bc8844b880902b06e51699e745216a77ee9a
simardo <simardo@gmail.com>
2023-05-03 21:33:48 -0400

    Day 06

---
+++ Day06/Part2.cs
@@ -0,0 +1,41 @@
+﻿namespace Day06 {
+
+    internal class Part2 {
+
+        public Part2() {
+        }
+
+        public string exec(string input) {
+            List<Dictionary<char, int>> corrected = new List<Dictionary<char, int>>();
+
+            string[] messages = input.Replace("\r", null).Split('\n');
+            foreach (string message in messages) {
+                char[] chars = message.ToCharArray();
+                if (corrected.Count == 0) {
+                    for (int i = 1; i <= chars.Length; i++) {
+                        corrected.Add(new Dictionary<char, int>());
+                    }
+                }
+                for (int i = 0; i < chars.Length; i++) {
+                    if (!corrected[i].ContainsKey(chars[i])) {
+                        corrected[i].Add(chars[i], 0);
+                    }
+                    corrected[i][chars[i]] = corrected[i][chars[i]] + 1;
+                }
+            }
+
+            string result = "";
+            foreach (Dictionary<char, int> c in corrected) {
+                KeyValuePair<char, int> max = c.Aggregate(new KeyValuePair<char, int>('z', int.MaxValue), (prev, kv) => {
+                    if (kv.Value < prev.Value) {
+                        return kv;
+                    }
+                    return prev;
+                });
+                result += max.Key;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Day06/Program.cs
@@ -0,0 +1,31 @@
+﻿using Day06;
+using Libs;
+
+string test1 = @"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
+
+Part1 p1 = new Part1();
+
+Console.WriteLine("Part1");
+Console.WriteLine(p1.exec(test1));
+Console.WriteLine(p1.exec(new StreamReader(Utils.GetInputFilename()).ReadToEnd()));
+
+Part2 p2 = new Part2();
+
+Console.WriteLine("Part2");
+Console.WriteLine(p2.exec(test1));
+Console.WriteLine(p2.exec(new StreamReader(Utils.GetInputFilename()).ReadToEnd()));
