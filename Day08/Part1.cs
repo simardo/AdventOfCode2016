a66e430b3e471ee795dbbf4bc45f868afd3ddaca
simardo <simardo@gmail.com>
2023-05-09 21:25:03 -0400

    Day 08

---
+++ Day08/Part1.cs
@@ -0,0 +1,106 @@
+﻿using Libs;
+using System.Text.RegularExpressions;
+
+namespace Day08 {
+
+    internal partial class Part1 {
+
+        [GeneratedRegex(@"rect (\d+)x(\d+)", RegexOptions.IgnoreCase)]
+        private partial Regex RectRegex();
+
+        [GeneratedRegex(@"rotate row y=(\d+) by (\d+)", RegexOptions.IgnoreCase)]
+        private partial Regex RotateRowRegex();
+
+        [GeneratedRegex(@"rotate column x=(\d+) by (\d+)", RegexOptions.IgnoreCase)]
+        private partial Regex RotateColumnRegex();
+
+        private int width;
+        private int height;
+
+        private List<OldNew<bool>> values;
+        private OldNew<bool>[][] display;
+
+        private void Apply() {
+            foreach (OldNew<bool> value in values) {
+                value.Apply();
+            }
+        }
+
+        public Part1(int width, int height) {
+            this.width = width;
+            this.height = height;
+
+            values = new List<OldNew<bool>>();
+            display = new OldNew<bool>[height][];
+        }
+
+        public int Exec(string input) {
+            List<string> sequences = new List<string>(input.Replace("\r", null).Split('\n'));
+
+            for (int row = 0; row < height; row++) {
+                display[row] = new OldNew<bool>[width];
+                for (int col = 0; col < width; col++) {
+                    OldNew<bool> value = new OldNew<bool>(false);
+                    display[row][col] = value;
+                    values.Add(value);
+                }
+            }
+
+            foreach (string sequence in sequences) {
+                Match m;
+                m = RectRegex().Match(sequence);
+                if (m.Success) {
+                    int w = Convert.ToInt32(m.Groups[1].Value);
+                    int h = Convert.ToInt32(m.Groups[2].Value);
+
+                    for (int x = 0; x < w; x++) {
+                        for (int y = 0; y < h; y++) {
+                            display[y][x].Value = true;
+                        }
+                    }
+                    Apply();
+
+                    continue;
+                }
+                m = RotateRowRegex().Match(sequence);
+                if (m.Success) {
+                    int y = Convert.ToInt32(m.Groups[1].Value);
+                    int b = Convert.ToInt32(m.Groups[2].Value) % width;
+
+                    for (int x = 0; x < width; x++) {
+                        int target = x + b;
+                        if (target >= width) {
+                            target = target - width;
+                        }
+                        display[y][target].Value = display[y][x].Value;
+                    }
+                    Apply();
+
+                    continue;
+                }
+                m = RotateColumnRegex().Match(sequence);
+                if (m.Success) {
+                    int x = Convert.ToInt32(m.Groups[1].Value);
+                    int b = Convert.ToInt32(m.Groups[2].Value) % height;
+
+                    for (int y = 0; y < height; y++) {
+                        int target = y + b;
+                        if (target >= height) {
+                            target = target - height;
+                        }
+                        display[target][x].Value = display[y][x].Value;
+                    }
+
+                    Apply();
+                    continue;
+                }
+            }
+
+            return values.Count(v => v.Value == true);
+        }
+
+        public string Render() {
+            return string.Join("\n", display.Select(row => string.Join("", row.Select(col => col.Value ? "#" : " "))));
+        }
+    }
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day08;
+using Libs;
+
+string test1 = @"rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1";
+
+Console.WriteLine("Part1");
+Console.WriteLine(new Part1(7, 3).Exec(test1));
+Part1 p1 = new Part1(50, 6);
+Console.WriteLine(p1.Exec(new StreamReader(Utils.GetInputFilename()).ReadToEnd()));
+Console.WriteLine(p1.Render());
